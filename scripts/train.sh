938c5ecd8fbaf4e20f42900d478d790319a53558
Yilun Chen <chenyilun12@utexas.edu>
2015-05-18 10:43:29 -0500

    add script/train.sh

---
+++ scripts/train.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# set -e
+
+if [ $# -lt 1 ]; then
+    echo "usage: $0 savepath"
+    exit
+fi
+
+LOGNAME=$1
+
+echo "==> Run server"
+/u/chen/robocup_libs/HFO/bin/start.py --offense 1 --defense 0 --headless &
+sleep 1
+echo "==> Run DQN"
+./dqn -save $LOGNAME -memory_threshold 1000
+sleep 1
+echo "==> Done Killing Server!"
+killall -9 rcssserver
